7d6a46bf8ab4ad4bf61a0d099044f7aeff814741
EC2 Default User <ec2-user@ip-172-31-19-177.ap-northeast-1.compute.internal>
2023-08-26 09:03:41 +0000

    migrate_bigint変更

--- db/migrate/20230604133817_create_equipment.rb
+++ db/migrate/20230604133817_create_equipment.rb
@@ -2,7 +2,7 @@ class CreateEquipment < ActiveRecord::Migration[6.1]
   def change
     create_table :equipment do |t|
       
-      t.integer :register_id,       null: false
+      t.bigint :register_id,       null: false
       t.integer  :reference_number, null: false
       t.string  :introduction,      null: false
       t.string  :location,          null: false
