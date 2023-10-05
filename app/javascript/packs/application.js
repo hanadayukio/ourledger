7e9425d08f7a01872daba373f35453288069317a
hanadayukio <124165299+hanadayukio@users.noreply.github.com>
2023-10-05 05:21:04 +0000

    scssをjavascript/stylesheetsからassets/stylesheetsへ

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,7 +13,6 @@ import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"; 
 // import '@fortawesome/fontawesome-free/js/all';
-//import "./boootstrap_custom";
 
 Rails.start()
 Turbolinks.start()
